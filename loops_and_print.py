090cb04852e71a60adb7b6729118c84bce81aa71
Camila Tobalina <162723051+camilatobalina@users.noreply.github.com>
2024-05-24 09:01:01 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,20 +1,16 @@
 def enumerate_list(list):
-    new_list=[]
-    a=0
-    for i in list:
-        if i!="":
-            new_indice=f'{a}. {i}'
-            a+=1
-            new_list.append(new_indice)
-    return new_list
-
+    lista1 = []
+    for posicion,elemento in enumerate(list):
+        if elemento != "":
+            largo = len(lista1)
+            lista1.append(f"{largo}. {elemento}")
+    return lista1
+    
 
 def enumerate_backwards(list):
-    new_list=[]
-    a=0
-    for i in list:
-        if i!="":
-            new_indice=f'{a}. {i[-1::-1]}'
-            a+=1
-            new_list.append(new_indice)
-    return new_list
+    lista1 = []
+    for posicion, elemento in enumerate(list):
+        if elemento != "":
+            largo = len(lista1)
+            lista1.append(f"{largo}. {elemento[::-1]}")
+    return lista1
